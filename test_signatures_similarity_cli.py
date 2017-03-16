9cd6c3b8ffa934d71c4df9c55406960fa137bc4e
Pankaj Ahuja <ray.pankaj@gmail.com>
2017-03-16 14:36:59 +0530

    Minor changes made to test_signatures_similarity CLI

--- test_signatures_similarity_cli.py
+++ test_signatures_similarity_cli.py
@@ -12,13 +12,13 @@ parser = argparse.ArgumentParser(description = 'Test similarities between two si
 parser.add_argument('-1','--one',required = True, help = 'First signature' )
 parser.add_argument('-2','--two',required = True, help = 'Second signature' )
 parser.add_argument('-p','--product',required = True, help = 'Product for which crash data is needed to be downloaded' )
-parser.add_argument('-t','--top', help = 'Number of top similar and different stack traces(Default 10)' )
+parser.add_argument('-t','--top', help = 'Number of top similar and different stack traces(Default 10)', default = 10, type = int )
 args = parser.parse_args()
 
 
 if __name__ == '__main__':
-    #download_data.download_crashes(days=1, product=args.product)
-    paths = download_data.get_paths(days=1, product=args.product)
+    download_data.download_crashes(days=3, product=args.product)
+    paths = download_data.get_paths(days=3, product=args.product)
     #paths = ['crashsimilarity_data/firefox-crashes-2016-11-09.json', 'crashsimilarity_data/firefox-crashes-2016-11-08.json', 'crashsimilarity_data/firefox-crashes-2016-11-07.json', 'crashsimilarity_data/firefox-crashes-2016-11-06.json', 'crashsimilarity_data/firefox-crashes-2016-11-05.json', 'crashsimilarity_data/firefox-crashes-2016-11-04.json', 'crashsimilarity_data/firefox-crashes-2016-11-03.json']
 
     corpus = crash_similarity.read_corpus(paths)
@@ -26,12 +26,10 @@ if __name__ == '__main__':
     model = crash_similarity.train_model(corpus)
 
     print(args.one + ' vs ' + args.two)
-    similarities = crash_similarity.signature_similarity(model, paths,'shutdownhang | js::GCMarker::processMarkStackTop','shutdownhang | js::GCMarker::processMarkStackTop')
-    #similarities = crash_similarity.signature_similarity(model, paths, args.one, args.two)
-    if(args.top == None):
-        args.top = 10
+    #similarities = crash_similarity.signature_similarity(model, paths,'shutdownhang | js::GCMarker::processMarkStackTop','shutdownhang | js::GCMarker::processMarkStackTop')
+    similarities = crash_similarity.signature_similarity(model, paths, args.one, args.two)
     print('Top ' + str(args.top))
-    for similarity in similarities[:int(args.top)]:
+    for similarity in similarities[:args.top]:
         print(u'%s\n%s\n%s\n' % (similarity[2], similarity[0], similarity[1]))
     print('Bottom ' + str(args.top))
     for similarity in similarities[-int(args.top):]:
