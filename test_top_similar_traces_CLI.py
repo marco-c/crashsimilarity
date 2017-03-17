78c36a71c16ecee5e808d5d5a8104ada44807bc1
Aditya Singh <aditya.dpsg.15@gmail.com>
2017-03-17 22:35:58 +0530

    removed whitespace

--- test_top_similar_traces_CLI.py
+++ test_top_similar_traces_CLI.py
@@ -15,21 +15,21 @@ parser.add_argument('--top', help='Number of top similar and different stack tra
 args = parser.parse_args()
 
 if __name__ == '__main__':
-  # downloads some data (e.g. the past 7 days)
-  download_data.download_crashes(days=7, product=args.product)
-  paths = download_data.get_paths(days=7, product=args.product)
-  
-  # reads the corpus
-  corpus = crash_similarity.read_corpus(paths)
-  
-  # trains the model on that data
-  model = crash_similarity.train_model(corpus)
-  
-  # gets the stack_trace corresponding to the crash_id (input by the user as an argument)
-  stack_trace = crash_similarity.get_stack_trace_from_crashid(args.crash_id)
-  
-  # returns the top similar stack traces (number of stack traces returned = args.top)
-  similarities = crash_similarity.top_similar_traces(model, corpus, stack_trace, args.top)
-  
-  for similarity in similarities:
+    # downloads some data (e.g. the past 7 days)
+    download_data.download_crashes(days=7, product=args.product)
+    paths = download_data.get_paths(days=7, product=args.product)
+
+    # reads the corpus
+    corpus = crash_similarity.read_corpus(paths)
+
+    # trains the model on that data
+    model = crash_similarity.train_model(corpus)
+
+    # gets the stack_trace corresponding to the crash_id (input by the user as an argument)
+    stack_trace = crash_similarity.get_stack_trace_from_crashid(args.crash_id)
+
+    # returns the top similar stack traces (number of stack traces returned = args.top)
+    similarities = crash_similarity.top_similar_traces(model, corpus, stack_trace, args.top)
+
+    for similarity in similarities:
     print(u'%s: <%s>\n' % ((corpus[similarity[0]].tags[1], similarity[1]), ' '.join(corpus[similarity[0]].words))
