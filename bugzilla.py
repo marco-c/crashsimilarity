4893303052130bac3f8aaaf5cabb32b6714726aa
Pankaj Ahuja <ray.pankaj@gmail.com>
2017-03-18 01:55:36 +0530

    Removed extra characters from signatures

--- bugzilla.py
+++ bugzilla.py
@@ -5,8 +5,8 @@ def get_signatures_from_bug(bug_id):
     url = 'https://bugzilla.mozilla.org/rest/bug'
     response = utils.get_with_retries(url, params={'id': bug_id})
     response.raise_for_status()
-    return [str(sig)[1: len(sig) - 1] for sig in response.json()['bugs'][0]['cf_crash_signature'].split('\r\n')]
+    return [str(sig)[2: len(sig) - 1].strip() for sig in response.json()['bugs'][0]['cf_crash_signature'].split('\r\n')]
 
 
 if __name__ == "__main__":
-    get_signatures_from_bug('1308863')
+    print(get_signatures_from_bug('1308863'))
