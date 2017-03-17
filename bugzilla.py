9c1d64867f3f9aafd822fd66666ca908dadf41da
Pankaj Ahuja <ray.pankaj@gmail.com>
2017-03-18 01:32:31 +0530

    Split signatures into a list

--- bugzilla.py
+++ bugzilla.py
@@ -5,7 +5,7 @@ def get_signatures_from_bug(bug_id):
     url = 'https://bugzilla.mozilla.org/rest/bug'
     response = utils.get_with_retries(url, params={'id': bug_id})
     response.raise_for_status()
-    return response.json()['bugs'][0]['cf_crash_signature']
+    return [str(sig)[1: len(sig) - 1] for sig in response.json()['bugs'][0]['cf_crash_signature'].split('\r\n')]
 
 
 if __name__ == "__main__":
