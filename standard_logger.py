644c26727a43d099787b6af1b277ac66ba49fc6b
Aditya Singh <aditya.dpsg.15@gmail.com>
2017-03-18 03:11:15 +0530

    Implemented Standard logger for logging.

---
+++ standard_logger.py
@@ -0,0 +1,18 @@
+import logging
+
+def login():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    # create a file handler
+    handler = logging.FileHandler('log_file.log')
+    handler.setLevel(logging.INFO)
+
+    # logging format
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    # add the handlers to the logger
+    logger.addHandler(handler)
+
+    logger.info('You are logged in!')
