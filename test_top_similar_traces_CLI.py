bf4d4c761c47c019c182d0c68693a14d0927041c
Aditya Singh <aditya.dpsg.15@gmail.com>
2017-03-17 21:47:52 +0530

    Removed unused imports

--- test_top_similar_traces_CLI.py
+++ test_top_similar_traces_CLI.py
@@ -8,8 +8,6 @@
 import download_data
 import crash_similarity
 import argparse
-import utils
-from datetime import timedelta
 
 parser = argparse.ArgumentParser(description='Returns the top ten similar stack traces')
 parser.add_argument('--crash_id', required=True, help='crash_id corresponding to the stack trace' )
