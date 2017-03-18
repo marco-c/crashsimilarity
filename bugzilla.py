eedda3cdd542f8ed531a13ddda9c83372f321779
Pankaj Ahuja <ray.pankaj@gmail.com>
2017-03-19 00:53:22 +0530

    Fixed minor syntax error

--- bugzilla.py
+++ bugzilla.py
@@ -5,7 +5,7 @@ def get_signatures_from_bug(bug_id):
     url = 'https://bugzilla.mozilla.org/rest/bug'
     response = utils.get_with_retries(url, params={'id': bug_id})
     response.raise_for_status()
-    return [sig.strip(' [@]) for sig in response.json()['bugs'][0]['cf_crash_signature'].split('\r\n')]
+    return [sig.strip(' [@]') for sig in response.json()['bugs'][0]['cf_crash_signature'].split('\r\n')]
 
 
 if __name__ == "__main__":
