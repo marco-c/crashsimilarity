6953094abf3d44cce28c815b85bb6d01d46e9697
Aditya Singh <aditya.dpsg.15@gmail.com>
2017-03-20 14:56:40 +0530

    Added Version and Travis Badges; Issue #46 (#47)
    * Create test_top_similar_traces_CLI.py
    
    * Update test_top_similar_traces_CLI.py
    
    * Create test_signature_coherence_CLI.py
    
    * Create last_trained.txt
    
    * Update crash_similarity.py
    
    * Update crash_similarity.py
    
    * Update crash_similarity.py
    
    * Update test_signature_coherence_CLI.py
    
    * Update test_top_similar_traces_CLI.py
    
    * Update crash_similarity.py
    
    * Update .gitignore
    
    * Removed last_trained.txt
    
    * Re-wrote the "check_if_model_trained_in_last_24hours()" to pass time_interval as a parameter.
    
    * Fixed code formatting
    
    * Re-wrote "check_if_model_trained_in_last_24hours()" 
    
    "check_if_model_trained_in_last_24hours()" now uses datetime.timedelta class.
    
    * Delete test_top_similar_traces_CLI.py
    
    * Removed training time check function.
    
    * Removed last_trained.txt from .gitignore
    
    * Removed unnecessary blank lines.
    
    * Removed unused imports
    
    * Update crash_similarity.py
    
    * Removed whitespaces
    
    * Removed changes
    
    * Added Version and Travis Badges
    
    * Made some changes to display image
    
    * Made some minor changes
    
    * Update README.md
    
    * Update README.md
    
    * Removed version badge

---
+++ test_signature_coherence_CLI.py
@@ -0,0 +1,38 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# CLI INTERFACE THAT EVALUATES THE SIMILARITY BETWEEN STACK TRACES IN A GIVEN SIGNATURE.
+
+import download_data
+import crash_similarity
+import argparse
+
+parser = argparse.ArgumentParser(description='Test Signature Coherence')
+parser.add_argument('--signature', required=True, help='Signature')
+parser.add_argument('--product', required=True, help='Product for which crash data is needed to be downloaded')
+parser.add_argument('--top', help='Number of top similar and different stack traces(Default 10)', default=10, type=int)
+args = parser.parse_args()
+
+if __name__ == '__main__':
+
+    # Downloads some data (e.g. the past 7 days)
+    download_data.download_crashes(days=7, product=args.product)
+    paths = download_data.get_paths(days=7, product=args.product)
+
+    # Reads the corpus
+    corpus = crash_similarity.read_corpus(paths)
+
+    # Trains the model on that data
+    model = crash_similarity.train_model(corpus)
+
+    # Evaluates the similarity between the stack traces in a given signature.
+    print(args.signature + ' \n')
+
+    similarities = crash_similarity.signature_similarity(model, paths, args.signature, args.signature)
+    print('Top ' + str(args.top))
+    for similarity in similarities[:args.top]:
+        print(u'%s\n%s\n%s\n' % (similarity[2], similarity[0], similarity[1]))
+    print('Bottom ' + str(args.top))
+    for similarity in similarities[-int(args.top):]:
+        print(u'%s\n%s\n%s\n' % (similarity[2], similarity[0], similarity[1]))
