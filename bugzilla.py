4541434ac0fa5819d7980f693f1e90ecdcf3860a
Pankaj Ahuja <ray.pankaj@gmail.com>
2017-03-23 00:44:38 +0530

    Fixes issue#55

--- bugzilla.py
+++ bugzilla.py
@@ -9,7 +9,15 @@ def get_signatures_from_bug(bug_id):
     url = 'https://bugzilla.mozilla.org/rest/bug'
     response = utils.get_with_retries(url, params={'id': bug_id})
     response.raise_for_status()
-    return [sig.strip(' [@]') for sig in response.json()['bugs'][0]['cf_crash_signature'].split('\r\n')]
+    signature = []
+    for sig in response.json()['bugs'][0]['cf_crash_signature'].split('\r\n'):
+        if sig[:3] == ' [@':
+            sig = sig[3:]
+        if sig[-1] == ']':
+            sig = sig[:-1]
+        sig.strip()
+        signature.append(sig)
+    return signature
 
 
 if __name__ == "__main__":
