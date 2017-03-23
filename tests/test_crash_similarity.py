c43fb7dc89b63d83be38f7923cde96e1d8e8bd2e
mansimarkaur <mansimarkaur.mks@gmail.com>
2017-03-24 02:56:41 +0530

    Added some more tests

--- tests/test_crash_similarity.py
+++ tests/test_crash_similarity.py
@@ -3,13 +3,15 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import unittest
+import json
 import crash_similarity
+import utils
 
 funcs = [('js::jit::MakeMRegExpHoistable ', 'js::jit::makemregexphoistable '), (' AppKit@0x7be82f ', ' appkit@0x'), (' __RtlUserThreadStart ', ' __rtluserthreadstart '), (' xul.dll@0x1ade7cf ', ' xul.dll@0x'), ('XUL@0x7bd20f', 'xul@0x'), ('libxul.so@0xe477b4 ', 'libxul.so@0x')]
 
 stack_trace = "js::GCMarker::processMarkStackTop | js::GCMarker::drainMarkStack | js::gc::GCRuntime::incrementalCollectSlice | js::gc::GCRuntime::gcCycle | js::gc::GCRuntime::collect | JS::StartIncrementalGC | nsJSContext::GarbageCollectNow | nsTimerImpl::Fire | nsTimerEvent::Run | nsThread::ProcessNextEvent | NS_ProcessPendingEvents | nsBaseAppShell::NativeEventCallback | nsAppShell::ProcessGeckoEvents | CoreFoundation@0xa74b0 | CoreFoundation@0x8861c | CoreFoundation@0x87b15 | CoreFoundation@0x87513 | HIToolbox@0x312ab | HIToolbox@0x310e0 | HIToolbox@0x30f15 | AppKit@0x476cc | AppKit@0x7be82f | CoreFoundation@0x9e3a1 | AppKit@0xc56609 | AppKit@0xc9e7f7 | AppKit@0xc9e387 | AppKit@0xc567a9 | AppKit@0xc5867b | AppKit@0xc57ccc | AppKit@0xc5a9c2 | AppKit@0x47c2ed | AppKit@0x47c304 | AppKit@0xcdcf03 | AppKit@0xc56e2b | AppKit@0xc579af | AppKit@0xcdcee2 | AppKit@0xc5e77b | AppKit@0xc9897a | AppKit@0xc9c88c | AppKit@0xc7f10e"
 
-clean_stack_traces = ['js::gcmarker::processmarkstacktop', 'js::gcmarker::drainmarkstack', 'js::gc::gcruntime::incrementalcollectslice', 'js::gc::gcruntime::gccycle', 'js::gc::gcruntime::collect', 'js::startincrementalgc', 'nsjscontext::garbagecollectnow', 'nstimerimpl::fire', 'nstimerevent::run', 'nsthread::processnextevent']
+clean_stack_trace = ['js::gcmarker::processmarkstacktop', 'js::gcmarker::drainmarkstack', 'js::gc::gcruntime::incrementalcollectslice', 'js::gc::gcruntime::gccycle', 'js::gc::gcruntime::collect', 'js::startincrementalgc', 'nsjscontext::garbagecollectnow', 'nstimerimpl::fire', 'nstimerevent::run', 'nsthread::processnextevent']
 
 
 class CrashSimilarityTest(unittest.TestCase):
@@ -21,13 +23,10 @@ class CrashSimilarityTest(unittest.TestCase):
         corpus = crash_similarity.read_corpus(self.paths)
         self.model = crash_similarity.train_model(corpus)
 
-        print('Model Trained with paths:\n{}'.format(self.paths))
-
     # Test if equal reports have distance 0 and different reports have difference greater than 0
     @unittest.expectedFailure
     def test_zero_dist_coherence(self):
         signature = 'mozilla::testZeroCoherence'
-        print('\nTesting Coherence with {} signature.'.format(signature))
 
         similarities = crash_similarity.signature_similarity(self.model, self.paths, signature, signature)
 
@@ -36,23 +35,19 @@ class CrashSimilarityTest(unittest.TestCase):
             if doc1 != doc2:
                 try:
                     self.assertTrue(dist > 0)
-                except AssertionError as e:
+                except AssertionError:
                     errors.append((doc1, doc2, dist))
             else:
                 try:
                     self.assertEqual(dist, 0)
-                except AssertionError as e:
+                except AssertionError:
                     errors.append((doc1, doc2, dist))
 
-        print('Number of errors:{}'.format(len(errors)))
-        for e in errors:
-            print('doc1: {}\ndoc2: {}\ndist: {}'.format(e[0], e[1], e[2]))
         self.assertEqual(len(errors), 0)
 
     # Test if reports with the same words in different order have distance different than zero
     @unittest.expectedFailure
     def test_order_similarity(self):
-        print('\nTesting if order changes document similarity.')
 
         signature1 = 'mozilla::testOrdem1'
         signature2 = 'mozilla::testOrdem2'
@@ -64,12 +59,7 @@ class CrashSimilarityTest(unittest.TestCase):
         doc_mid1, doc_mid2, dist_mid = similarity_mid[0]
         doc_end1, doc_end2, dist_end = similarity_end[0]
 
-        if dist_mid >= dist_end:
-            print('{}\n{}\n{}'.format(doc_mid1, doc_mid2, dist_mid))
-            print('{}\n{}\n{}'.format(doc_end1, doc_end2, dist_end))
-
         self.assertTrue(dist_mid < dist_end)
-        print('\n')
 
     def test_clean_func(self):
         for f, expected in funcs:
@@ -78,7 +68,7 @@ class CrashSimilarityTest(unittest.TestCase):
     def test_preprocess_returns_clean_stack_traces(self):
         resp = crash_similarity.preprocess(stack_trace)
         self.assertEqual(len(resp), 10)
-        self.assertEqual(resp, clean_stack_traces)
+        self.assertEqual(resp, clean_stack_trace)
 
     def test_should_skip_returns_correct_boolean_value(self):
         for f in funcs:
@@ -86,6 +76,19 @@ class CrashSimilarityTest(unittest.TestCase):
                 self.assertTrue(crash_similarity.should_skip(f))
             else:
                 self.assertFalse(crash_similarity.should_skip(f))
+
+    def test_read_corpus(self):
+        resp = crash_similarity.read_corpus(self.paths)
+        self.assertEqual(type(resp), list)
+        self.assertEqual(len(resp), 378)
+
+    def test_get_stack_traces_for_signature(self):
+        signature = 'js::GCMarker::processMarkStackTop'
+        resp = crash_similarity.get_stack_traces_for_signature(self.paths, signature)
+        for line in utils.read_files(self.paths):
+            data = json.loads(line)
+            if data['signature'] == signature:
+                assert data['proto_signature'] in resp
 
 
 if __name__ == '__main__':
