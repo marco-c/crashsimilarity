8eb1ebafeead639eb4120ce34c8a2bf6001f1c16
mansimarkaur <mansimarkaur.mks@gmail.com>
2017-03-24 22:48:06 +0530

    Added more tests

--- tests/test_crash_similarity.py
+++ tests/test_crash_similarity.py
@@ -90,6 +90,12 @@ class CrashSimilarityTest(unittest.TestCase):
             if data['signature'] == signature:
                 assert data['proto_signature'] in resp
 
+    def test_get_stack_trace_for_uuid(self):
+        uuid = '90dcebb6-f711-4a8b-9e68-5abf72161109'
+        proto_signature = 'js::GCMarker::processMarkStackTop | js::GCMarker::drainMarkStack | js::gc::GCRuntime::incrementalCollectSlice | js::gc::GCRuntime::gcCycle | js::gc::GCRuntime::collect | JS::StartIncrementalGC | nsJSContext::GarbageCollectNow | nsTimerImpl::Fire | nsTimerEvent::Run | nsThread::ProcessNextEvent | NS_ProcessPendingEvents | nsBaseAppShell::NativeEventCallback | nsAppShell::ProcessGeckoEvents | CoreFoundation@0xa74b0 | CoreFoundation@0x8861c | CoreFoundation@0x87b15 | CoreFoundation@0x87513 | HIToolbox@0x312ab | HIToolbox@0x310e0 | HIToolbox@0x30f15 | AppKit@0x476cc | AppKit@0x7be82f | CoreFoundation@0x9e3a1 | AppKit@0xc56609 | AppKit@0xc9e7f7 | AppKit@0xc9e387 | AppKit@0xc567a9 | AppKit@0xc5867b | AppKit@0xc57ccc | AppKit@0xc5a9c2 | AppKit@0x47c2ed | AppKit@0x47c304 | AppKit@0xcdcf03 | AppKit@0xc56e2b | AppKit@0xc579af | AppKit@0xcdcee2 | AppKit@0xc5e77b | AppKit@0xc9897a | AppKit@0xc9c88c | AppKit@0xc7f10e'
+        resp = crash_similarity.get_stack_trace_for_uuid(uuid)
+        self.assertEqual(resp, proto_signature)
+
 
 if __name__ == '__main__':
     unittest.main()
