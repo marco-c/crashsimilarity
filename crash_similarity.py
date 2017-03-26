90ba6c8cfcc16a4be157cd6eb508f5621b42c40b
Ibrahim Sharaf ElDen <ibrahimsharafelden@gmail.com>
2017-03-26 23:40:58 +0200

    finalize word2vec integration

--- crash_similarity.py
+++ crash_similarity.py
@@ -12,10 +12,13 @@ import time
 import gensim
 import numpy as np
 
+import pyximport
+from pyemd import emd
+
 import download_data
 import utils
+from download_data import download_stack_traces_for_signature
 
-import pyximport
 pyximport.install()
 
 
@@ -57,37 +60,15 @@ def read_corpus(fnames, embedding_algo='doc2vec'):
                 elems.append(processed)
 
     if embedding_algo == 'doc2vec':
-        return [gensim.models.doc2vec.TaggedDocument(trace, [i, signature]) for i, (trace, signature) in enumerate(elems)]
+        return [gensim.models.doc2vec.TaggedDocument(trace, [i, signature]) for i, (trace, signature) in
+                enumerate(elems)]
     if embedding_algo == 'word2vec':
         return elems
 
 
-def get_stack_trace_from_crashid(crash_id):
-    url = 'https://crash-stats.mozilla.com/api/ProcessedCrash'
-    params = {
-        'crash_id': crash_id
-    }
-    res = utils.get_with_retries(url, params)
-    return res.json()['proto_signature']
-
-
 def get_stack_traces_for_signature(fnames, signature, traces_num=100):
-    traces = set()
+    traces = download_stack_traces_for_signature(signature, traces_num)
 
-    # query stack traces online
-    url = 'https://crash-stats.mozilla.com/api/SuperSearch'
-    params = {
-        'signature': '=' + signature,
-        '_facets': ['proto_signature'],
-        '_facets_size': traces_num,
-        '_results_number': 0
-    }
-    res = utils.get_with_retries(url, params)
-    records = res.json()['facets']['proto_signature']
-    for record in records:
-        traces.add(record['term'])
-
-    # query stack traces from downloaded data
     for line in utils.read_files(fnames):
         data = json.loads(line)
         if data['signature'] == signature:
@@ -101,11 +82,13 @@ def get_stack_trace_for_uuid(uuid):
     return data['proto_signature']
 
 
-def train_model(corpus, embedding_algo='doc2vec'):
-    if os.path.exists('stack_traces_' + embedding_algo + '_model_pickle') and embedding_algo == 'doc2vec':
-        return gensim.models.Doc2Vec.load('stack_traces_' + embedding_algo + '_model_pickle')
-    elif os.path.exists('stack_traces_' + embedding_algo + '_model_pickle') and embedding_algo == 'word2vec':
-        return gensim.models.Word2Vec.load('stack_traces_' + embedding_algo + '_model_pickle')
+def train_model(corpus,embedding_algo='doc2vec'):
+    if os.path.exists('stack_traces_' + embedding_algo + '_model.pickle') and embedding_algo == 'doc2vec'\
+            and os.path.exists('stack_traces_doc2vec_model.pickle.docvecs.doctag_syn0.npy'):
+        return gensim.models.Doc2Vec.load('stack_traces_' + embedding_algo + '_model.pickle')
+
+    if os.path.exists('stack_traces_' + embedding_algo + '_model.pickle') and embedding_algo == 'word2vec':
+        return gensim.models.Word2Vec.load('stack_traces_' + embedding_algo + '_model.pickle')
     random.shuffle(corpus)
 
     print('CORPUS LENGTH: ' + str(len(corpus)))
@@ -122,24 +105,61 @@ def train_model(corpus, embedding_algo='doc2vec'):
         model = gensim.models.Word2Vec(size=100, window=8, iter=20, workers=workers)
 
     model.build_vocab(corpus)
+    print("Vocab Length{}".format(len(model.wv.vocab)))
 
     t = time.time()
     print('Training model...')
     model.train(corpus)
     print('Model trained in ' + str(time.time() - t) + ' s.')
 
-    model.save('stack_traces_' + embedding_algo + '_model_pickle')
+    model.save('stack_traces_' + embedding_algo + '_model.pickle')
 
     return model
 
 
-def top_similar_traces(model, corpus, stack_trace, top=10):
+# create distance_matrix using precalculate cosine distance from rwmd
+def create_distance_matrix(model, dictionary, docset, all_distances):
+    distances = np.zeros((len(dictionary), len(dictionary)), dtype=np.double)
+    for j, w in dictionary.items():
+        if w in docset:
+            distances[:all_distances.shape[1], j] = all_distances[model.wv.vocab[w].index].transpose()
+
+    return distances
+
+
+# Code moodified from https://github.com/RaRe-Technologies/gensim/blob/4f0e2ae/gensim/models/keyedvectors.py#L339
+def wmdistance(model, words1, words2, all_distances):
+    dictionary = gensim.corpora.Dictionary(documents=[words1, words2])
+    vocab_len = len(dictionary)
+
+    # create bag of words from document
+    def create_bow(doc):
+        norm_bow = np.zeros(vocab_len, dtype=np.double)
+        bow = dictionary.doc2bow(doc)
+
+        for idx, count in bow:
+            norm_bow[idx] = count / float(len(doc))
+
+        return norm_bow
+
+    bow1 = create_bow(words1)
+    bow2 = create_bow(words2)
+
+    docset = set(words2)
+    distances = create_distance_matrix(model, dictionary, docset, all_distances)
+
+    assert sum(distances) != 0
+
+    return emd(bow1, bow2, distances)
+
+
+def top_similar_traces(model, corpus, stack_trace, top=10, embedding_algo='doc2vec'):
     model.init_sims(replace=True)
 
     similarities = []
 
     words_to_test = preprocess(stack_trace)
-    words_to_test_clean = [w for w in words_to_test if w in model]
+    words_to_test_clean = [w for w in np.unique(words_to_test).tolist() if w in model]
 
     # TODO: Test if a first sorting with the average vectors is useful.
     '''
@@ -147,13 +167,19 @@ def top_similar_traces(model, corpus, stack_trace, top=10):
     sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
     '''
 
-    all_distances = 1 - np.dot(model.syn0norm, model.syn0norm[[model.vocab[word].index for word in words_to_test_clean]].transpose())
-    print(all_distances.shape)
+    # Cos-similarity
+    all_distances = np.array(1.0 - np.dot(model.wv.syn0norm, model.wv.syn0norm[
+        [model.wv.vocab[word].index for word in words_to_test_clean]].transpose()), dtype=np.double)
 
+    # Relaxed Word Mover's Distance for selecting
     t = time.time()
     distances = []
     for doc_id in range(0, len(corpus)):
-        doc_words = [model.vocab[word].index for word in corpus[doc_id].words if word in model]
+        if embedding_algo == 'doc2vec':
+            doc_words = [model.wv.vocab[word].index for word in corpus[doc_id].words if word in model]
+        elif embedding_algo == 'word2vec':
+            doc_words = [model.wv.vocab[word].index for word in corpus[doc_id] if word in model]
+
         if len(doc_words) != 0:
             word_dists = all_distances[doc_words]
             rwmd = max(np.sum(np.min(word_dists, axis=0)), np.sum(np.min(word_dists, axis=1)))
@@ -176,7 +202,15 @@ def top_similar_traces(model, corpus, stack_trace, top=10):
             break
 
         # TODO: replace this with inline code (to avoid recalculating the distances).
-        wmd = model.wmdistance(words_to_test, corpus[doc_id].words)
+        # wmd = model.wmdistance(words_to_test, corpus[doc_id].words)                      # uses euclidian distance
+
+        if embedding_algo == 'doc2vec':
+            doc_words_clean = [w for w in corpus[doc_id].words if w in model]
+        elif embedding_algo == 'word2vec':
+            doc_words_clean = [w for w in corpus[doc_id] if w in model]
+
+        wmd = wmdistance(model, words_to_test_clean, doc_words_clean, all_distances)  # uses cosine distance
+
         j = bisect.bisect(confirmed_distances, wmd)
         confirmed_distances.insert(j, wmd)
         confirmed_distances_ids.insert(j, doc_id)
@@ -189,24 +223,26 @@ def top_similar_traces(model, corpus, stack_trace, top=10):
 
 
 def signature_similarity(model, paths, signature1, signature2):
+    model.init_sims(replace=True)
     traces1 = get_stack_traces_for_signature(paths, signature1)
     traces2 = get_stack_traces_for_signature(paths, signature2)
 
     similarities = []
-
     already_processed = set()
 
     for doc1 in traces1:
-        words1 = [word for word in preprocess(doc1) if word in model]
+        words1 = np.unique([word for word in preprocess(doc1) if word in model]).tolist()
+        distances = np.array(1.0 - np.dot(model.wv.syn0norm, model.wv.syn0norm[
+            [model.wv.vocab[word].index for word in words1]].transpose()), dtype=np.double)
 
         for doc2 in traces2:
             words2 = [word for word in preprocess(doc2) if word in model]
 
-            if words1 == words2 or frozenset([frozenset(words1), frozenset(words2)]) in already_processed:
+            if words1 == words2 or frozenset([tuple(words1), tuple(words2)]) in already_processed:
                 continue
-            already_processed.add(frozenset([frozenset(words1), frozenset(words2)]))
+            already_processed.add(frozenset([tuple(words1), tuple(words2)]))
 
-            similarities.append((doc1, doc2, model.wmdistance(words1, words2)))
+            similarities.append((doc1, doc2, wmdistance(model, words1, words2, distances)))
 
     return sorted(similarities, key=lambda v: v[2])
 
@@ -214,15 +250,10 @@ def signature_similarity(model, paths, signature1, signature2):
 if __name__ == '__main__':
     # download_data.download_crashes(days=7, product='Firefox')
     # paths = download_data.get_paths(days=7, product='Firefox')
-    paths = ['crashsimilarity_data/firefox-crashes-2016-11-09.json.gz',
-             'crashsimilarity_data/firefox-crashes-2016-11-08.json.gz',
-             'crashsimilarity_data/firefox-crashes-2016-11-07.json.gz',
-             'crashsimilarity_data/firefox-crashes-2016-11-06.json.gz',
-             'crashsimilarity_data/firefox-crashes-2016-11-05.json.gz',
-             'crashsimilarity_data/firefox-crashes-2016-11-04.json.gz',
-             'crashsimilarity_data/firefox-crashes-2016-11-03.json.gz']
+    paths = ['crashsimilarity_data/firefox-crashes-2016-11-09.json.gz', 'crashsimilarity_data/firefox-crashes-2016-11-08.json.gz', 'crashsimilarity_data/firefox-crashes-2016-11-07.json.gz', 'crashsimilarity_data/firefox-crashes-2016-11-06.json.gz', 'crashsimilarity_data/firefox-crashes-2016-11-05.json.gz', 'crashsimilarity_data/firefox-crashes-2016-11-04.json.gz', 'crashsimilarity_data/firefox-crashes-2016-11-03.json.gz']
 
     corpus = read_corpus(paths)
 
     model = train_model(corpus)
+
     print(dict([(model.index2word[i], similarity) for i, similarity in enumerate(model.similar_by_word('igdumd32.dll@0x', topn=False))])['igdumd64.dll@0x'])
