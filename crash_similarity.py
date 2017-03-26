0288b40338c7efa83e05d6d38962b244816e1db1
Aditya Singh <aditya.dpsg.15@gmail.com>
2017-03-26 23:52:48 +0530

    Removed basic.config calls

--- crash_similarity.py
+++ crash_similarity.py
@@ -21,7 +21,6 @@ import utils
 from download_data import download_stack_traces_for_signature
 
 pyximport.install()
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def clean_func(func):
