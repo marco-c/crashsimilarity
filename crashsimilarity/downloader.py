0dd056e33bec956d2e413889930cba135f986718
Ibrahim Sharaf <ibrahimsharafelden@gmail.com>
2018-02-14 19:33:32 +0200

    Review comments

--- crashsimilarity/downloader.py
+++ crashsimilarity/downloader.py
@@ -1,10 +1,10 @@
 import os
 import logging
+from datetime import timedelta
 
 import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3 import Retry
-from datetime import timedelta
 
 from crashsimilarity import utils
 
